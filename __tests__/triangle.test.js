22c01ea01999d32ad1b9c1ddb7bbaa3fe22193b3
ChrisNakayama <88105917+ChrisNakayama@users.noreply.github.com>
2021-09-01 09:57:17 -0700

    add testing capacity

--- __tests__/triangle.test.js
+++ __tests__/triangle.test.js
@@ -1,1 +1,6 @@
-nameOfFileToBeTested.test.js
+import Triangle from './../src/triangle.js';
+
+test('should correctly determine whether three lengths make an isosceles triangle', () => {
+  const isocTriangle = new Triangle(5,5,7)
+  expect(isocTriangle.checkType()).toEqual("isosceles triangle");
+});
